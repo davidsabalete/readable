6f59749c909faf65bd7cc15edfd4cb737dabfa91
David Sabalete <dsabalete@gmail.com>
2017-11-26 23:51:44 +0100

    Comment to stateless functional component

--- frontend/src/components/Comment.js
+++ frontend/src/components/Comment.js
@@ -5,42 +5,31 @@ import { votePostCommentAsync } from '../actions/comments'
 import CommentButtons from './CommentButtons'
 import { withRouter } from 'react-router'
 
+const Comment = (props) => (
+    <div className="comment">
+        <CommentButtons comment={props.comment} />
+        <blockquote>
+            <p className="date-info">On {new Date(props.comment.timestamp).toString().substr(0, 16)}
+                <strong>{props.comment.author}</strong> said:</p>
+            <cite>{props.comment.body}</cite>
+        </blockquote>
 
-class Comment extends React.Component {
+        <div className="commentMetadata">
+            <span className="badge badge-pill badge-primary">{props.comment.voteScore} votes </span> {" "}
+            <FontAwesome name="thumbs-o-up" onClick={() => {
+                props.votePostCommentAsync(props.comment.id, 'upVote', () => {
+                    props.history.push(props.match.url)
+                })
+            }} />{" "}
+            <FontAwesome name="thumbs-o-down" onClick={() => {
+                props.votePostCommentAsync(props.comment.id, 'downVote', () => {
+                    props.history.push(props.match.url)
+                })
+            }} />
+        </div>
+    </div>
+)
 
-    refresh() {
-        const { url } = this.props.match
-        this.props.history.push(url)
-    }
-
-    render() {
-        const { comment, votePostCommentAsync } = this.props
-        const { id, timestamp, body, author, voteScore } = comment
-        return (
-            <div className="comment">
-                <CommentButtons comment={this.props.comment} />
-                <blockquote>
-                    <p className="date-info">On {new Date(timestamp).toString().substr(0, 16)} <strong>{author}</strong> said:</p>
-                    <cite>{body}</cite>
-                </blockquote>
-
-                <div className="commentMetadata">
-                    <span className="badge badge-pill badge-primary">{voteScore} votes </span> {" "}
-                    <FontAwesome name="thumbs-o-up" onClick={() => {
-                        votePostCommentAsync(id, 'upVote', () => {
-                            this.refresh()
-                        })
-                    }} />{" "}
-                    <FontAwesome name="thumbs-o-down" onClick={() => {
-                        votePostCommentAsync(id, 'downVote', () => {
-                            this.refresh()
-                        })
-                    }} />
-                </div>
-            </div>
-        )
-    }
-}
 const mapStateToProps = ({ post, comments }) => {
     return {
         post,
